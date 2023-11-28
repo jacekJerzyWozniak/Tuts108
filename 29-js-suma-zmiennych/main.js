b9f6c05a6e6916f32f7ff8e506cf01135c422fd2
Jacol <jacekjwozniak@gmail.com>
2023-11-28 21:00:17 +0100

    29-js-suma-zmiennych, console.log(add());

--- 29-js-suma-zmiennych/main.js
+++ 29-js-suma-zmiennych/main.js
@@ -1,11 +1,48 @@
 
 
-let firstNumber = 32;
-let secondNumber = 404;
+// proszę mi napisać dlaczego ten pierwszy kod działa, a ten drugi już nie. Namęczyłem się jak cholera i w ogóle to chyba przez przypadek udało mnie się poprawnie wykonać to zadanie. Kod drugi, zaczynający się w linii 26, jest dokładnie napisany tak jak w zadaniu #48164, ale za chiny ludowe nie mogę go ruszyć, w konsoli wyskakuje NaN :/... Kod pierwszy również nie działa kiedy w linii 8 i 9 dodam "let" przed nazwami. Proszę mi to wytłumaczyć
+
 
 function add(firstNumber, secondNumber) {
+
+    firstNumber = 32;
+    secondNumber = 404;
+
 
     let suma = firstNumber + secondNumber;
 
     return suma;
 }
+
+
+console.log(add());
+
+
+
+
+
+
+
+// let firstNumber = 32;
+// let secondNumber = 404;
+
+// function add(firstNumber, secondNumber) {
+
+
+//     let suma = firstNumber + secondNumber;
+
+//     return suma;
+// }
+
+
+// console.log(add());
+
+
+
+
+
+
+
+
+
+
